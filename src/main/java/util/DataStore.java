795744315ad7b93eea58117250e3479034c87b68
Maksim <bogdanenko1996@gmail.com>
2024-02-05 17:26:49 +0300

    Сделал обьект по паттерну Singleton. Перенес статические методы в методы обьекта. Обновил метод getSupportPhrases() - возврящает копию состояния, чтобы не нарушать инкапсуляцию.

--- src/main/java/util/DataStore.java
+++ src/main/java/util/DataStore.java
@@ -1,27 +1,47 @@
 package util;
 
 import java.util.HashSet;
+import java.util.Map;
 import java.util.Random;
 import java.util.Set;
+import java.util.concurrent.ConcurrentHashMap;
 
 public class DataStore {
-    private static final Set<String> SUPPORT_PHRASES = new HashSet<>();
+    private static DataStore insance;
+    private static final Object stub = new Object();
 
-    static {
-        SUPPORT_PHRASES.add("У тебя все получится!");
-        SUPPORT_PHRASES.add("Еще чуть-чуть");
-        SUPPORT_PHRASES.add("Сегодня ты на высоте ;)");
+    public static DataStore getInstance() {
+        if (insance == null)
+            synchronized (DataStore.class) {
+                if (insance == null) {
+                    insance = new DataStore();
+                }
+            }
+        return insance;
     }
 
-    public static String getRandomSupportPhrase() {
-        int randomIndex = new Random().nextInt(SUPPORT_PHRASES.size());
-        return SUPPORT_PHRASES.stream().skip(randomIndex).findFirst().orElse("Помощь всегда рядом!");
+
+    private final Map<String, Object> supportPhrases;
+
+    private DataStore() {
+        this.supportPhrases = new ConcurrentHashMap<>();
+        supportPhrases.put("У тебя все получится!", stub);
+        supportPhrases.put("Еще чуть-чуть", stub);
+        supportPhrases.put("Сегодня ты на высоте ;)", stub);
     }
 
-    public static void addSupportPhrase(String phrase) {
-        SUPPORT_PHRASES.add(phrase);
+
+    public String getRandomSupportPhrase() {
+        int randomIndex = new Random().nextInt(supportPhrases.size());
+        return supportPhrases.keySet().stream().skip(randomIndex).findFirst().orElse("Помощь всегда рядом!");
     }
-    public static Set<String> getSupportPhrases(){
-        return SUPPORT_PHRASES;
+
+    public void addSupportPhrase(String phrase) {
+        supportPhrases.put(phrase, stub);
     }
+
+    public Set<String> getSupportPhrases() {
+        return new HashSet<>(supportPhrases.keySet());
+    }
+
 }

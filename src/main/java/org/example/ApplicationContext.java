f1f3b57941541f74a35dce2ee83adb5057ffdbb1
Maksim <bogdanenko1996@gmail.com>
2024-02-07 17:04:10 +0300

    ApplicationContext - отвечает за инициализацию и хранение обьектов, задекларированных в @Config классах

---
+++ src/main/java/org/example/ApplicationContext.java
@@ -0,0 +1,66 @@
+package org.example;
+
+import org.example.annotation.Configuration;
+import org.example.service.SupportService;
+import org.reflections.Reflections;
+
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.stream.Stream;
+
+public class ApplicationContext {
+    public static void main(String[] args) {
+        ApplicationContext context = new ApplicationContext();
+        context.getInstance(SupportService.class);
+    }
+
+    private final Reflections reflection;
+    private final Map<Class<?>, Object> INSTANCE_BY_CLASS;
+
+    public ApplicationContext(String packageToScan) {
+        this.INSTANCE_BY_CLASS = new HashMap<>();
+        this.reflection = new Reflections(packageToScan);
+        init();
+    }
+
+    public ApplicationContext() {
+        this("org.example");
+    }
+
+    private void init() {
+        reflection.getTypesAnnotatedWith(Configuration.class).stream()
+                .map(this::createInstanceByClass)
+                .forEach(configInstance -> getMethodsWithoutObjectsMethods(configInstance)
+                        .forEach(method -> initBeanByBeanMethod(method, configInstance)));
+    }
+
+
+    public <T> T getInstance(Class<T> clazz) {
+        return (T) this.INSTANCE_BY_CLASS.get(clazz);
+    }
+
+    private Stream<Method> getMethodsWithoutObjectsMethods(Object instance) {
+        return Arrays.stream(instance.getClass().getMethods()).filter(method -> !method.getDeclaringClass().equals(Object.class));
+    }
+
+    private <T> T createInstanceByClass(Class<T> clazz) {
+        try {
+            return clazz.getConstructor().newInstance();
+        } catch (InstantiationException | NoSuchMethodException | InvocationTargetException |
+                 IllegalAccessException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    private void initBeanByBeanMethod(Method beanMethod, Object configInstance) {
+        Class<?> returnType = beanMethod.getReturnType();
+        try {
+            INSTANCE_BY_CLASS.put(returnType, beanMethod.invoke(configInstance));
+        } catch (IllegalAccessException | InvocationTargetException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}

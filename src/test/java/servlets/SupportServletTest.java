f2656c7987365102cb028ec145e1c117977163c0
Maksim <bogdanenko1996@gmail.com>
2024-02-07 17:11:29 +0300

    Обновил логику тестов с учетом SupportService, добавил тест для init метода.

--- src/test/java/servlets/SupportServletTest.java
+++ src/test/java/servlets/SupportServletTest.java
@@ -1,28 +1,24 @@
 package servlets;
 
-import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
-import org.junit.jupiter.api.BeforeEach;
+import org.example.service.SupportService;
+import org.example.servlets.SupportServlet;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
 import org.mockito.Mock;
 
-import org.mockito.MockedStatic;
 import org.mockito.junit.jupiter.MockitoExtension;
-import util.DataStore;
 
 import java.io.IOException;
 import java.io.StringWriter;
 import java.io.PrintWriter;
 import java.io.BufferedReader;
 import java.io.StringReader;
-import java.util.Set;
 
-import static org.junit.jupiter.api.Assertions.assertTrue;
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.Mockito.when;
-import static org.mockito.Mockito.mockStatic;
 import static org.mockito.Mockito.doNothing;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.times;
@@ -31,59 +27,51 @@ import static org.mockito.Mockito.anyInt;
 
 @ExtendWith(MockitoExtension.class)
 class SupportServletTest {
-    private SupportServlet supportServlet;
+
     @Mock
     private HttpServletResponse resp;
     @Mock
     private HttpServletRequest req;
-    @Mock
-    private DataStore dataStore;
 
-    @BeforeEach
-    public void init() {
-        this.supportServlet = new SupportServlet();
-    }
+    @Mock
+    private SupportService supportService;
+    @InjectMocks
+    private SupportServlet supportServlet;
 
     @Test
-    public void doGetTest() throws ServletException, IOException {
+    public void do_get_should_write_support_phrase_to_response() throws IOException {
         StringWriter respStringWriter = new StringWriter();
         PrintWriter respWriter = new PrintWriter(respStringWriter);
         String supportPhrase = "Test phrase";
 
-        try(MockedStatic<DataStore> mockedDataStore = mockStatic(DataStore.class)){
-            mockedDataStore.when(DataStore::getInstance).thenReturn(dataStore);
-            when(dataStore.getRandomSupportPhrase()).thenReturn(supportPhrase);
-            when(resp.getWriter()).thenReturn(respWriter);
 
-            supportServlet.doGet(req, resp);
+        when(supportService.getSupportPhrase()).thenReturn(supportPhrase);
+        when(resp.getWriter()).thenReturn(respWriter);
 
-            verify(resp, times(1)).getWriter();
-            assertEquals(supportPhrase, respStringWriter.toString());
-        }
+        supportServlet.doGet(req, resp);
+
+        verify(resp, times(1)).getWriter();
+        assertEquals(supportPhrase, respStringWriter.toString());
+
     }
 
     @Test
-    public void doPostTest() throws ServletException, IOException {
+    public void do_post_should_add_new_phrase_from_req() throws IOException {
         String newSupportPhrase = "Everything will be fine!";
         BufferedReader reqReader = new BufferedReader(new StringReader(newSupportPhrase));
 
         when(req.getContentType()).thenReturn("text/plain");
         when(req.getReader()).thenReturn(reqReader);
-
-        Set<String> beforePost = DataStore.getInstance().getSupportPhrases();
+        doNothing().when(supportService).addSupportPhrase(newSupportPhrase);
 
         supportServlet.doPost(req, resp);
 
-        Set<String> afterPost = DataStore.getInstance().getSupportPhrases();
-        afterPost.removeAll(beforePost);
-
         verify(req, times(1)).getReader();
-        assertEquals(1, afterPost.size());
-        assertTrue(afterPost.contains(newSupportPhrase));
+        verify(supportService, times(1)).addSupportPhrase(newSupportPhrase);
     }
 
     @Test
-    public void doEmptyPostTest() throws ServletException, IOException {
+    public void do_empty_post_should_rsponse_wtih_error() throws IOException {
         String newSupportPhrase = "";
         BufferedReader reqReader = new BufferedReader(new StringReader(newSupportPhrase));
 
@@ -97,7 +85,7 @@ class SupportServletTest {
     }
 
     @Test
-    public void doPostWithWrongContentTypeTest() throws ServletException, IOException {
+    public void do_post_with_wrong_content_type_should_resopnse_wtih_error() throws IOException {
         String newSupportPhrase = "{ 'phrase' : 'new phrase'}";
         BufferedReader reqReader = new BufferedReader(new StringReader(newSupportPhrase));
 
@@ -109,4 +97,15 @@ class SupportServletTest {
 
         verify(resp, times(1)).sendError(HttpServletResponse.SC_UNSUPPORTED_MEDIA_TYPE);
     }
+
+    @Test
+    void init_should_initialize_support_service() {
+        SupportServlet servlet = new SupportServlet();
+
+        servlet.init();
+
+        assertNotNull(servlet.getSupportService());
+
+    }
+
 }

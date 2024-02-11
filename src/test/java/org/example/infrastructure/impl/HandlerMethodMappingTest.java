5917613590e3ae7a859e1b532b9ddbf53116dc0c
Maksim <bogdanenko1996@gmail.com>
2024-02-11 14:00:11 +0300

    Добавил тесты для DispatcherServlet/ HandlerMethodMapping

---
+++ src/test/java/org/example/infrastructure/impl/HandlerMethodMappingTest.java
@@ -0,0 +1,58 @@
+package org.example.infrastructure.impl;
+
+
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.example.exception.infrastructure.HandlerNotFoundException;
+import org.example.infrastructure.HandlerMethod;
+import org.example.infrastructure.MappingRegistry;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.io.IOException;
+import java.lang.reflect.InvocationTargetException;
+
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.doThrow;
+
+@ExtendWith(MockitoExtension.class)
+class HandlerMethodMappingTest {
+    @InjectMocks
+    private HandlerMethodMapping handlerMethodMapping;
+    @Mock
+    private MappingRegistry mappingRegistry;
+    @Mock
+    private HttpServletRequest request;
+    @Mock
+    private HttpServletResponse response;
+    @Mock
+    private HandlerMethod handlerMethod;
+
+    @Test
+    public void invokeHandler_should_invoke_handlerMethod_when_it_is_present() throws InvocationTargetException, IllegalAccessException, IOException, HandlerNotFoundException {
+        when(request.getContextPath()).thenReturn("/help-service");
+        when(request.getPathInfo()).thenReturn("/v1/support");
+        when(request.getMethod()).thenReturn("GET");
+        when(mappingRegistry.get("/help-service/v1/supportGET")).thenReturn(handlerMethod);
+
+        handlerMethodMapping.invokeHandler(request, response);
+
+        verify(handlerMethod, times(1)).invoke(request, response);
+    }
+
+    @Test
+    public void invokeHandler_should_throw_HandlerNotFoundExcetption_when_there_is_no_handler() throws InvocationTargetException, IllegalAccessException, HandlerNotFoundException {
+        when(request.getContextPath()).thenReturn("/help-service");
+        when(request.getPathInfo()).thenReturn("/v1/support");
+        when(request.getMethod()).thenReturn("GET");
+        doThrow(HandlerNotFoundException.class).when(mappingRegistry).get("/help-service/v1/supportGET");
+
+        assertThrows(HandlerNotFoundException.class, ()-> handlerMethodMapping.invokeHandler(request, response));
+    }
+}
---
+++ src/test/java/org/example/servlets/DispatcherServletTest.java
@@ -0,0 +1,65 @@
+package org.example.servlets;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.example.exception.infrastructure.HandlerNotFoundException;
+import org.example.infrastructure.HandlerMapping;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.io.IOException;
+import java.lang.reflect.InvocationTargetException;
+
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.doNothing;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.doThrow;
+
+@ExtendWith(MockitoExtension.class)
+class DispatcherServletTest {
+    @InjectMocks
+    private DispatcherServlet dispatcherServlet;
+    @Mock
+    private HandlerMapping handlerMapping;
+    @Mock
+    private HttpServletRequest request;
+    @Mock
+    private HttpServletResponse response;
+
+
+    @Test
+    public void init_should_inject_hadnlerMapping() throws ServletException {
+        dispatcherServlet.setHandlerMapping(null);
+
+        dispatcherServlet.init();
+
+        assertNotNull(dispatcherServlet.getHandlerMapping());
+
+    }
+    @Test
+    public void service_should_redirect_req_to_handler() throws HandlerNotFoundException, InvocationTargetException, IllegalAccessException, ServletException, IOException {
+
+        doNothing().when(handlerMapping).invokeHandler(any(HttpServletRequest.class), any(HttpServletResponse.class));
+
+        dispatcherServlet.service(request, response);
+
+        verify(handlerMapping, times(1)).invokeHandler(request, response);
+    }
+
+    @Test
+    public void service_should_res_404_error_when_handler_not_found() throws HandlerNotFoundException, InvocationTargetException, IllegalAccessException, ServletException, IOException {
+        doThrow(HandlerNotFoundException.class).when(handlerMapping).invokeHandler(any(HttpServletRequest.class), any(HttpServletResponse.class));
+
+        dispatcherServlet.service(request, response);
+
+        verify(handlerMapping, times(1)).invokeHandler(request, response);
+        verify(response, times(1)).sendError(HttpServletResponse.SC_NOT_FOUND);
+    }
+
+}

bf40ca8a1d3f758203626d166df595321e9c758a
Maksim <bogdanenko1996@gmail.com>
2024-02-11 14:03:54 +0300

    Добавил  регистрацию SupportController

--- src/main/java/org/example/config/SupportConfig.java
+++ src/main/java/org/example/config/SupportConfig.java
@@ -1,6 +1,9 @@
 package org.example.config;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.example.annotation.Configuration;
+import org.example.controller.SupportController;
+import org.example.controller.impl.SupportControllerImpl;
 import org.example.repository.SupportPhraseRepository;
 import org.example.repository.impl.SupportPhrasePhraseRepositoryImpl;
 import org.example.service.SupportService;
@@ -13,7 +16,11 @@ public class SupportConfig {
         return new SupportPhrasePhraseRepositoryImpl();
     }
 
-    public SupportService supportService(){
-        return new SupportServiceImpl(supportPhraseRepository());
+    public SupportService supportService(SupportPhraseRepository repository){
+        return new SupportServiceImpl(repository);
+    }
+
+    public SupportController supportController(SupportService supportService, ObjectMapper objectMapper){
+        return new SupportControllerImpl(supportService, objectMapper);
     }
 }

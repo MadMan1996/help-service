a892a775885def3910accac5bee506be37bc9d0c
Maksim <bogdanenko1996@gmail.com>
2024-02-18 16:07:48 +0300

    Перенес слои controller, service, repository, entity, dto из предыдущего модуля. Обновил логику добавления supportPhrase с учетом in-memory-broker

---
+++ spring-implementation/src/main/java/org/example/controller/SupportController.java
@@ -0,0 +1,38 @@
+package org.example.controller;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import org.example.Publisher;
+import org.example.dto.SupportPhraseDto;
+import org.example.service.SupportService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.io.IOException;
+
+@RestController
+@RequestMapping("/help-service/v1")
+public class SupportController {
+    private final SupportService supportService;
+    private final ObjectMapper objectMapper;
+    private final Publisher<SupportPhraseDto> supportPhrasePublisher;
+
+    public SupportController(SupportService supportService, ObjectMapper objectMapper, Publisher<SupportPhraseDto> publisher) {
+        this.supportService = supportService;
+        this.objectMapper = objectMapper;
+        this.supportPhrasePublisher = publisher;
+    }
+
+    @GetMapping("/support")
+    public ResponseEntity<SupportPhraseDto> getSupport() throws IOException {
+        SupportPhraseDto supportPhraseDto = objectMapper.convertValue(supportService.getSupportPhrase(), SupportPhraseDto.class);
+        return ResponseEntity.status(200).body(supportPhraseDto);
+    }
+
+    @PostMapping("/support")
+    public ResponseEntity<Void> addSupport(@RequestBody SupportPhraseDto phraseDto) throws IOException {
+        supportService.addSupportPhrase(phraseDto);
+        return ResponseEntity.status(200).build();
+    }
+
+}

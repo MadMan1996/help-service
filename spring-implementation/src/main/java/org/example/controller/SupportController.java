12c84c9f4c6d663180ad991d7df560bcef798e7e
Maksim <bogdanenko1996@gmail.com>
2024-02-18 21:15:42 +0300

    Удалил неиспльзуемые компоненты

--- spring-implementation/src/main/java/org/example/controller/SupportController.java
+++ spring-implementation/src/main/java/org/example/controller/SupportController.java
@@ -2,25 +2,20 @@ package org.example.controller;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 
-import org.example.Publisher;
 import org.example.dto.SupportPhraseDto;
 import org.example.service.SupportService;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-
-import java.io.IOException;
 
 @RestController
 @RequestMapping("/help-service/v1")
 public class SupportController {
     private final SupportService supportService;
     private final ObjectMapper objectMapper;
-    private final Publisher<SupportPhraseDto> supportPhrasePublisher;
 
-    public SupportController(SupportService supportService, ObjectMapper objectMapper, Publisher<SupportPhraseDto> publisher) {
+    public SupportController(SupportService supportService, ObjectMapper objectMapper) {
         this.supportService = supportService;
         this.objectMapper = objectMapper;
-        this.supportPhrasePublisher = publisher;
     }
 
     @GetMapping("/support")

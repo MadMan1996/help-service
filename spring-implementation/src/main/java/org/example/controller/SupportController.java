252fcc8be4fbff783eb369a661a825795e510848
Maksim <bogdanenko1996@gmail.com>
2024-02-25 19:04:28 +0300

    Удалил излишний маппинг в ResponseEntity

--- spring-implementation/src/main/java/org/example/controller/SupportController.java
+++ spring-implementation/src/main/java/org/example/controller/SupportController.java
@@ -2,32 +2,32 @@ package org.example.controller;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 
+import lombok.RequiredArgsConstructor;
 import org.example.dto.SupportPhraseDto;
+import org.example.entity.SupportPhrase;
 import org.example.service.SupportService;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestBody;
+
 
 @RestController
 @RequestMapping("/help-service/v1")
+@RequiredArgsConstructor
 public class SupportController {
     private final SupportService supportService;
     private final ObjectMapper objectMapper;
 
-    public SupportController(SupportService supportService, ObjectMapper objectMapper) {
-        this.supportService = supportService;
-        this.objectMapper = objectMapper;
-    }
-
     @GetMapping("/support")
-    public ResponseEntity<SupportPhraseDto> getSupport() {
-        SupportPhraseDto supportPhraseDto = objectMapper.convertValue(supportService.getSupportPhrase(), SupportPhraseDto.class);
-        return ResponseEntity.status(200).body(supportPhraseDto);
+    public SupportPhraseDto getSupport() {
+       return objectMapper.convertValue(supportService.getSupportPhrase(), SupportPhraseDto.class);
     }
 
     @PostMapping("/support")
-    public ResponseEntity<Void> addSupport(@RequestBody SupportPhraseDto phraseDto) {
-        supportService.addSupportPhrase(phraseDto);
-        return ResponseEntity.status(200).build();
+    public void addSupport(@RequestBody SupportPhraseDto phraseDto) {
+        supportService.addSupportPhrase(objectMapper.convertValue(phraseDto, SupportPhrase.class));
     }
 
 }

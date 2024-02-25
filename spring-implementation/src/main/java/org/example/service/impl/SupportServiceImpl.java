400234db66417e7380dbfe9bb2833a8279f1d6be
Maksim <bogdanenko1996@gmail.com>
2024-02-25 19:10:11 +0300

    Обновил логику SupportService

--- spring-implementation/src/main/java/org/example/service/impl/SupportServiceImpl.java
+++ spring-implementation/src/main/java/org/example/service/impl/SupportServiceImpl.java
@@ -1,30 +1,36 @@
 package org.example.service.impl;
 
-import org.example.Publisher;
-import org.example.dto.SupportPhraseDto;
+import lombok.RequiredArgsConstructor;
+import org.example.config.property.KafkaSupportServiceProp;
 import org.example.entity.SupportPhrase;
 import org.example.repository.SupportPhraseRepository;
 import org.example.service.SupportService;
+import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+import java.util.Random;
+
 @Service
+@RequiredArgsConstructor
 public class SupportServiceImpl implements SupportService {
-
+    private final List<KafkaTemplate<String, Object>> kafkaTemplates;
     private final SupportPhraseRepository supportPhraseRepository;
-    private final Publisher<SupportPhraseDto> supportPhrasePublisher;
-
-    public SupportServiceImpl(SupportPhraseRepository supportPhraseRepository, Publisher<SupportPhraseDto> supportPhrasePublisher) {
-        this.supportPhraseRepository = supportPhraseRepository;
-        this.supportPhrasePublisher = supportPhrasePublisher;
-    }
+    private final KafkaSupportServiceProp props;
 
     @Override
     public SupportPhrase getSupportPhrase(){
-        return supportPhraseRepository.getRandom();
+        List<Long> ids = supportPhraseRepository.getIds();
+        Long randomEntityIndex = ids.stream().skip(new Random().nextInt(ids.size())).findFirst().orElseGet(()->1L);
+        return supportPhraseRepository.findById(randomEntityIndex).orElseGet(()->new SupportPhrase("Default support phrase"));
+
     }
     @Override
-    public void addSupportPhrase(SupportPhraseDto phraseDto){
-        supportPhrasePublisher.sendEvent(phraseDto);
+    public void addSupportPhrase(SupportPhrase supportPhrase){
+        SupportPhrase entity = supportPhraseRepository.save(supportPhrase);
+        for(var producer : kafkaTemplates){
+            producer.send(props.getTopic(), supportPhrase);
+        }
     }
 
 
